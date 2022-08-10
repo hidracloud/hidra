b51c7feedcd83e66ebfe0b0092d981dd0f65d005
José Carlos <JoseCarlosGarcia95@users.noreply.github.com>
2022-08-10 12:32:10 +0200

    This branch improve browser experience. (#28)
    * This branch improve browser experience.
    
    * browser: clean up after finisihing
    
    * bugfixing: cleanup browser after screenshot

--- pkg/scenarios/browser/browser.go
+++ pkg/scenarios/browser/browser.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/chromedp/chromedp"
 	"github.com/hidracloud/hidra/v2/pkg/models"
@@ -14,7 +16,8 @@ import (
 // Scenario Represent an browser scenario
 type Scenario struct {
 	models.Scenario
-	ctx context.Context
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 // RCA generate RCAs for scenario
@@ -42,7 +45,7 @@ func (b *Scenario) urlShouldBe(ctx context.Context, c map[string]string) ([]mode
 func (b *Scenario) textShouldBe(ctx context.Context, c map[string]string) ([]models.Metric, error) {
 	var text string
 	err := chromedp.Run(b.ctx,
-		chromedp.Text(c["selector"], &text),
+		chromedp.Text(c["selector"], &text, selector2By(c["selector"])),
 	)
 
 	if err != nil {
@@ -57,7 +60,7 @@ func (b *Scenario) textShouldBe(ctx context.Context, c map[string]string) ([]mod
 
 func (b *Scenario) sendKeys(ctx context.Context, c map[string]string) ([]models.Metric, error) {
 	err := chromedp.Run(b.ctx,
-		chromedp.SendKeys(c["selector"], c["keys"]),
+		chromedp.SendKeys(c["selector"], c["keys"], selector2By(c["selector"])),
 	)
 
 	if err != nil {
@@ -67,9 +70,26 @@ func (b *Scenario) sendKeys(ctx context.Context, c map[string]string) ([]models.
 	return nil, nil
 }
 
+func selector2By(selector string) func(*chromedp.Selector) {
+	opt := chromedp.BySearch
+
+	if selector != "" {
+		switch selector {
+		case "bySearch":
+			opt = chromedp.BySearch
+		case "byID":
+			opt = chromedp.ByID
+		case "byQuery":
+			opt = chromedp.ByQuery
+		}
+	}
+
+	return opt
+}
+
 func (b *Scenario) waitVisible(ctx context.Context, c map[string]string) ([]models.Metric, error) {
 	err := chromedp.Run(b.ctx,
-		chromedp.WaitVisible(c["selector"]),
+		chromedp.WaitVisible(c["selector"], selector2By(c["selector"])),
 	)
 
 	if err != nil {
@@ -81,7 +101,7 @@ func (b *Scenario) waitVisible(ctx context.Context, c map[string]string) ([]mode
 
 func (b *Scenario) click(ctx context.Context, c map[string]string) ([]models.Metric, error) {
 	err := chromedp.Run(b.ctx,
-		chromedp.Click(c["selector"], chromedp.NodeVisible),
+		chromedp.Click(c["selector"], selector2By(c["selector"])),
 	)
 
 	if err != nil {
@@ -103,6 +123,54 @@ func (b *Scenario) navigateTo(ctx context.Context, c map[string]string) ([]model
 	return nil, nil
 }
 
+func (b *Scenario) takeScreenshot(ctx context.Context, c map[string]string) ([]models.Metric, error) {
+	var screenshot []byte
+	var err error
+
+	if c["selector"] == "" {
+		err = chromedp.Run(b.ctx,
+			chromedp.FullScreenshot(&screenshot, 90),
+		)
+	} else {
+		err = chromedp.Run(b.ctx,
+			chromedp.Screenshot(c["selector"], &screenshot),
+		)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// save screenshot bytes to file
+	f, err := os.OpenFile("screenshot.png", os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = f.Write(screenshot)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (b *Scenario) wait(ctx context.Context, c map[string]string) ([]models.Metric, error) {
+	sleep, _ := time.ParseDuration(c["sleep"])
+
+	err := chromedp.Run(b.ctx,
+		chromedp.Sleep(sleep),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
 // Description return scenario description
 func (b *Scenario) Description() string {
 	return "Run scenario in real browser"
@@ -115,6 +183,11 @@ func (b *Scenario) Close() {
 		log.Println(err)
 	}
 	b.ctx.Done()
+
+	// keep browser open
+	if os.Getenv("DEBUG") == "" {
+		b.cancel()
+	}
 }
 
 // Init initialize scenario
@@ -122,8 +195,15 @@ func (b *Scenario) Init() {
 	b.StartPrimitives()
 
 	// Initialize chrome context
-	b.ctx, _ = chromedp.NewContext(
-		context.Background(),
+	initialCtx := context.Background()
+
+	// If debug env is set, use debug mode
+	if os.Getenv("DEBUG") != "" {
+		initialCtx, _ = chromedp.NewExecAllocator(initialCtx, append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))...)
+	}
+
+	b.ctx, b.cancel = chromedp.NewContext(
+		initialCtx,
 	)
 
 	b.RegisterStep("navigateTo", models.StepDefinition{
@@ -147,6 +227,7 @@ func (b *Scenario) Init() {
 		Params: []models.StepParam{
 			{Name: "text", Optional: false, Description: "The text to check"},
 			{Name: "selector", Optional: false, Description: "The selector to check"},
+			{Name: "searchMethod", Optional: true, Description: "The search method to use. "},
 		},
 		Fn: b.textShouldBe,
 	})
@@ -156,6 +237,7 @@ func (b *Scenario) Init() {
 		Params: []models.StepParam{
 			{Name: "keys", Optional: false, Description: "The keys to send to the selector. "},
 			{Name: "selector", Optional: false, Description: "The selector to send the keys to. "},
+			{Name: "searchMethod", Optional: true, Description: "The search method to use. "},
 		},
 		Fn: b.sendKeys,
 	})
@@ -164,6 +246,7 @@ func (b *Scenario) Init() {
 		Description: "It waits until the selector is visible",
 		Params: []models.StepParam{
 			{Name: "selector", Optional: false, Description: "The selector to wait for. "},
+			{Name: "searchMethod", Optional: true, Description: "The search method to use. "},
 		},
 		Fn: b.waitVisible,
 	})
@@ -172,9 +255,27 @@ func (b *Scenario) Init() {
 		Description: "It clicks on a selector",
 		Params: []models.StepParam{
 			{Name: "selector", Optional: false, Description: "The selector to click. "},
+			{Name: "searchMethod", Optional: true, Description: "The search method to use. "},
 		},
 		Fn: b.click,
 	})
+
+	b.RegisterStep("takeScreenshot", models.StepDefinition{
+		Description: "It takes a screenshot",
+		Params: []models.StepParam{
+			{Name: "selector", Optional: true, Description: "The selector to take the screenshot of. "},
+		},
+		Fn: b.takeScreenshot,
+	})
+
+	b.RegisterStep("wait", models.StepDefinition{
+		Description: "It waits for a given amount of time",
+		Params: []models.StepParam{
+			{Name: "sleep", Optional: false, Description: "The amount of time to wait. "},
+		},
+		Fn: b.wait,
+	})
+
 }
 
 func init() {
