2f3c19c6db0d813cbf49ab42ab5de40870b6fddd
josecarlos <hola@josecarlos.me>
2022-09-08 16:42:59 +0200

    not more needed
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
 const Version = "v2.13.0-11-g324a983"
-const BuildDate = "2022-09-08T14:30:24Z"
+const BuildDate = "jueves,  8 de septiembre de 2022, 14:41:26 UTC"
 const Commit = "324a9834fbb13dab7e8e72e3769b2e1800929243"
 const Branch = "v3"
