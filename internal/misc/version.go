d43cd802a5366074730b4541bcf87211cc9d0e65
josecarlos <hola@josecarlos.me>
2022-09-11 17:36:57 +0200

    install tasks
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
-const Version = "v2.13.0-23-gcf2b4ae"
-const BuildDate = "2022-09-11T15:33:50Z"
-const Commit = "cf2b4ae2d77800bfd49ec346f13112ee18908f0d"
+const Version = "v2.13.0-24-gbdb7334"
+const BuildDate = "2022-09-11T15:36:58Z"
+const Commit = "bdb7334c881f4c6e6d84a6c39edc7eb4b2cdeb41"
 const Branch = "v3"
