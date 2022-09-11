72156c32c2b28d996d14a578575a6be95bc495af
josecarlos <hola@josecarlos.me>
2022-09-11 14:05:50 +0200

    update version
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
-const Version = "v2.13.0-18-gd9da17e"
-const BuildDate = "2022-09-11T10:15:13Z"
-const Commit = "d9da17e1311c33d1006d29d8935b28b53cdf0aac"
+const Version = "v2.13.0-19-gf50e696"
+const BuildDate = "2022-09-11T12:05:20Z"
+const Commit = "f50e696f9341566273688ceea89a156f90071d3c"
 const Branch = "v3"
