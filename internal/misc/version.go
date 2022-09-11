c7f6378999ca5ec0e1205f9b7c5d2bb53db3aa79
josecarlos <hola@josecarlos.me>
2022-09-11 10:40:17 +0200

    update grafana dashboard
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
-const Version = "v2.13.0-14-g023374b"
-const BuildDate = "2022-09-09T08:34:45Z"
-const Commit = "023374b5fb458e274a3d1ddb0cf3ea2e5184f0f7"
+const Version = "v2.13.0-16-g1646662"
+const BuildDate = "2022-09-11T08:40:12Z"
+const Commit = "1646662b9e5a0bd1d1ff955c915a74e121430052"
 const Branch = "v3"
