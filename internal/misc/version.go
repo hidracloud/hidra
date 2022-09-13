301dafe1a128f7f22f8a19e39e2a336aba6bd942
josecarlos <hola@josecarlos.me>
2022-09-13 19:48:28 +0200

    fetch all tags for goreleasrr
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
-const Version = "v2.13.0-28-g279e64b"
-const BuildDate = "2022-09-13T17:44:18Z"
-const Commit = "279e64b534e183a7c0ceaeacb5073ea0410d3a0f"
+const Version = "v3.0.0-alpha0"
+const BuildDate = "2022-09-13T17:48:30Z"
+const Commit = "4f50a0571b3b3ff0a6582ee0512353386b1788e2"
 const Branch = "v3"
