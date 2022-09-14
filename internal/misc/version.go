30308caad8a889875a3bd254c7ecfcd7437ffc34
josecarlos <hola@josecarlos.me>
2022-09-14 19:05:05 +0200

    add version.go updater
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
-const Version = "v3.0.0-alpha0"
-const BuildDate = "2022-09-13T17:48:30Z"
-const Commit = "4f50a0571b3b3ff0a6582ee0512353386b1788e2"
+const Version = "v3.0.0-alpha0-1-g301dafe"
+const BuildDate = "2022-09-14T17:05:06Z"
+const Commit = "301dafe1a128f7f22f8a19e39e2a336aba6bd942"
 const Branch = "v3"
