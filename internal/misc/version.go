8511c6e5b04acc788ebcb4ff03862e38953aea43
josecarlos <hola@josecarlos.me>
2022-09-14 19:10:19 +0200

    remove autoupdater
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,5 @@
 package misc
-const Version = "v3.0.0-alpha0-1-g301dafe"
-const BuildDate = "2022-09-14T17:05:06Z"
-const Commit = "301dafe1a128f7f22f8a19e39e2a336aba6bd942"
+const Version = "v3.0.0-alpha1"
+const BuildDate = "2022-09-14T17:10:20Z"
+const Commit = "30308caad8a889875a3bd254c7ecfcd7437ffc34"
 const Branch = "v3"
