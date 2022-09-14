87e3d94b8cab0079354c9f95595a6eab55386ac3
josecarlos <hola@josecarlos.me>
2022-09-14 22:36:05 +0200

    improve version generation
    Signed-off-by: josecarlos <hola@josecarlos.me>

--- internal/misc/version.go
+++ internal/misc/version.go
@@ -1,5 +1,6 @@
 package misc
-const Version = "v3.0.0-alpha2"
-const BuildDate = "2022-09-14T17:14:56Z"
-const Commit = "8511c6e5b04acc788ebcb4ff03862e38953aea43"
-const Branch = "v3"
+
+var Version = "dev"
+var BuildDate = "n/a"
+var Commit = "n/a"
+var Branch = "n/a"
